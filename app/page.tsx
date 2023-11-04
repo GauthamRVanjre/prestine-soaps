aca2a9a976f0e744e9b3fe768489a0226d726976
Gautham Vanjre <vanjregautham@gmail.com>
2023-11-04 23:38:02 +0530

    setup of prisma with Mongodb

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import { useState } from "react";
-
 export default function Home() {
-  const [name, setName] = useState("");
-
   return (
-    <main>
-      <input type="text"></input>
-      <Button>Click me</Button>
-    </main>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   );
 }
